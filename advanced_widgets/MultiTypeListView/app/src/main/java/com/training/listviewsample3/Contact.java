d62fd2eedc472b55977e97dda939e93b8b572018
mahdi <mahdi@infusiona-apps.com>
2014-10-29 14:33:36 +0300

    refactor the trainig chapters structure

---
+++ advanced_widgets/MultiTypeListView/app/src/main/java/com/training/listviewsample3/Contact.java
@@ -0,0 +1,12 @@
+package com.training.listviewsample3;
+
+public class Contact {
+	public String name;
+	public int pictureId;
+
+	public Contact(String name, int pictureId) {
+		this.name = name;
+		this.pictureId = pictureId;
+		
+	}
+}
---
+++ advanced_widgets/MultiTypeListView/app/src/main/java/com/training/listviewsample3/Separator.java
@@ -0,0 +1,15 @@
+package com.training.listviewsample3;
+
+public class Separator {
+	String title;
+
+	public Separator(String title) {
+		this.title = title;
+
+	}
+
+	public String getTitle() {
+		return this.title;
+
+	}
+}
