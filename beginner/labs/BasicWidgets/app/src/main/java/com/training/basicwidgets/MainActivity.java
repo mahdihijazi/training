d8042176f95214917ac33006879db7f7d60d98f8
mahdi <mahdi@infusiona-apps.com>
2015-03-20 20:21:19 +0200

    cleanup the code

--- beginner/labs/BasicWidgets/app/src/main/java/com/training/basicwidgets/MainActivity.java
+++ beginner/labs/BasicWidgets/app/src/main/java/com/training/basicwidgets/MainActivity.java
@@ -1,9 +1,7 @@
 package com.training.basicwidgets;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class MainActivity extends ActionBarActivity {
