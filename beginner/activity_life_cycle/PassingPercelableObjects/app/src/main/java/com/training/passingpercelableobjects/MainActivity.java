a5e51290b20ea1517a50ab14ed42b5a27c2a2f12
mahdi <mahdi@infusiona-apps.com>
2015-04-11 13:39:03 +0300

    format the code

--- beginner/activity_life_cycle/PassingPercelableObjects/app/src/main/java/com/training/passingpercelableobjects/MainActivity.java
+++ beginner/activity_life_cycle/PassingPercelableObjects/app/src/main/java/com/training/passingpercelableobjects/MainActivity.java
@@ -1,8 +1,8 @@
 package com.training.passingpercelableobjects;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
--- beginner/activity_life_cycle/PassingPercelableObjects/app/src/main/java/com/training/passingpercelableobjects/WelcomeActivity.java
+++ beginner/activity_life_cycle/PassingPercelableObjects/app/src/main/java/com/training/passingpercelableobjects/WelcomeActivity.java
@@ -1,21 +1,20 @@
 package com.training.passingpercelableobjects;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
 public class WelcomeActivity extends ActionBarActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_welcome);
-		Contact contact = getIntent().getParcelableExtra("contact");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Contact contact = getIntent().getParcelableExtra("contact");
 
-		TextView textView = (TextView) findViewById(R.id.welcome_me);
-		textView.setText("Name: " + contact.name + "\nMobile: "
-				+ contact.mobile);
+        TextView textView = (TextView) findViewById(R.id.welcome_me);
+        textView.setText("Name: " + contact.name + "\nMobile: "
+                + contact.mobile);
 
-	}
+    }
 }
