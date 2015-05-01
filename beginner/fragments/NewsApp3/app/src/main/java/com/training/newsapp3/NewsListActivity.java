567926d71c5e757c422ca9bcd87d56fc3d113754
mahdi <mahdi@infusiona-apps.com>
2015-05-01 23:28:19 +0300

    complete NewsApp step3

--- beginner/fragments/NewsApp3/app/src/main/java/com/training/newsapp3/NewsListActivity.java
+++ beginner/fragments/NewsApp3/app/src/main/java/com/training/newsapp3/NewsListActivity.java
@@ -7,21 +7,40 @@ import android.support.v7.app.AppCompatActivity;
 
 public class NewsListActivity extends AppCompatActivity {
 
+    private boolean mTwoPane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_list);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, NewsListFragment.newInstance())
-                    .commit();
+        if (findViewById(R.id.newsitem_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
         }
+
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+
     }
 
     public void articleSelected(Article article) {
-        Intent intent = new Intent(NewsListActivity.this, NewsItemDetailActivity.class);
-        intent.putExtra(NewsItemDetailFragment.ARG_ITEM, article);
-        startActivity(intent);
+        if (mTwoPane) {
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(NewsItemDetailFragment.ARG_ITEM, article);
+            NewsItemDetailFragment fragment = new NewsItemDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.newsitem_detail_container, fragment)
+                    .commit();
+
+        } else {
+            Intent intent = new Intent(NewsListActivity.this, NewsItemDetailActivity.class);
+            intent.putExtra(NewsItemDetailFragment.ARG_ITEM, article);
+            startActivity(intent);
+        }
+
     }
 
 }
