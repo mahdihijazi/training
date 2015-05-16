4f1158248e684a217f811747407855270452b5f6
mahdi <mahdi@infusiona-apps.com>
2015-05-16 09:32:03 +0300

    fix sample 1

--- beginner/toolbar/ToolbarSample1/app/src/main/java/com/training/toolbarsample1/MainActivity.java
+++ beginner/toolbar/ToolbarSample1/app/src/main/java/com/training/toolbarsample1/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,14 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Toast.makeText(this, "About", Toast.LENGTH_LONG).show();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                return true;
+
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refresh", Toast.LENGTH_LONG).show();
+
+                return true;
+
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show();
+
+                return true;
+
         }
 
         return super.onOptionsItemSelected(item);
