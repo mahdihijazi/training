5f9791972512255d1a2c835dce1ffb322b7f7a10
mahdi <mahdi@infusiona-apps.com>
2015-05-16 11:33:56 +0300

    add view pager lab solution

---
+++ beginner/labs/solutions/ViewPagerLab/app/src/main/java/com/training/viewpagerlab/MainActivity.java
@@ -0,0 +1,46 @@
+package com.training.viewpagerlab;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int NUMBER_OF_PAGES = 5;
+
+    private ViewPager mViewPager;
+    private MyFragmentPagerAdapter mMyFragmentPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mMyFragmentPagerAdapter = new MyFragmentPagerAdapter(
+                getSupportFragmentManager());
+        mViewPager.setAdapter(mMyFragmentPagerAdapter);
+
+    }
+
+    private static class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int index) {
+            return WelcomeFragment.newInstance();
+        }
+
+        @Override
+        public int getCount() {
+            return NUMBER_OF_PAGES;
+        }
+    }
+}
---
+++ beginner/labs/solutions/ViewPagerLab/app/src/main/java/com/training/viewpagerlab/WelcomeFragment.java
@@ -0,0 +1,58 @@
+package com.training.viewpagerlab;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+public class WelcomeFragment extends Fragment {
+
+    public static WelcomeFragment newInstance() {
+        WelcomeFragment fragment = new WelcomeFragment();
+        return fragment;
+    }
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_welcome, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            Toast.makeText(getActivity(), "Fragment Refresh", Toast.LENGTH_LONG).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+
+
+}
