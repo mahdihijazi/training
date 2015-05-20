878490fe1384f9111066d5d34832a84f40e41cd8
mahdi <mahdi@infusiona-apps.com>
2015-05-20 22:53:15 +0300

    add constants sample 2

---
+++ beginner/database/ConstantsSample2/app/src/main/java/com/training/constantssample2/ConstantsFragment.java
@@ -0,0 +1,194 @@
+package com.training.constantssample2;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class ConstantsFragment extends Fragment implements
+        DialogInterface.OnClickListener {
+    private DatabaseHelper db = null;
+    private CursorAdapter cursorAdapter;
+    private ListView listView;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+        db = new DatabaseHelper(getActivity());
+        new LoadCursorTask().execute();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_constants, container,
+                false);
+        listView = (ListView) view.findViewById(R.id.list);
+        listView.setOnItemLongClickListener(itemLongClickListener);
+
+        return view;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        cursorAdapter.getCursor().close();
+        db.close();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                add();
+                return (true);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void add() {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View addView = inflater.inflate(R.layout.add_edit, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle(R.string.add_title).setView(addView)
+                .setPositiveButton(R.string.ok, this)
+                .setNegativeButton(R.string.cancel, null).show();
+    }
+
+    public void onClick(DialogInterface di, int whichButton) {
+        ContentValues values = new ContentValues(2);
+        AlertDialog dlg = (AlertDialog) di;
+        EditText title = (EditText) dlg.findViewById(R.id.title);
+        EditText value = (EditText) dlg.findViewById(R.id.value);
+
+        values.put(DatabaseHelper.TITLE, title.getText().toString());
+        values.put(DatabaseHelper.VALUE, value.getText().toString());
+
+        new InsertTask().execute(values);
+    }
+
+    private Cursor doQuery() {
+        return (db.getReadableDatabase().rawQuery("SELECT _id, title, value "
+                + "FROM constants ORDER BY title", null));
+    }
+
+    private class LoadCursorTask extends AsyncTask<Void, Void, Void> {
+        private Cursor constantsCursor = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            constantsCursor = doQuery();
+            constantsCursor.getCount();
+
+            return (null);
+        }
+
+        @SuppressLint("NewApi")
+        @SuppressWarnings("deprecation")
+        @Override
+        public void onPostExecute(Void arg0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                cursorAdapter = new SimpleCursorAdapter(getActivity(),
+                        R.layout.row, constantsCursor, new String[]{
+                        DatabaseHelper.TITLE, DatabaseHelper.VALUE},
+                        new int[]{R.id.title, R.id.value}, 0);
+            } else {
+                cursorAdapter = new SimpleCursorAdapter(getActivity(),
+                        R.layout.row, constantsCursor, new String[]{
+                        DatabaseHelper.TITLE, DatabaseHelper.VALUE},
+                        new int[]{R.id.title, R.id.value});
+            }
+
+            listView.setAdapter(cursorAdapter);
+
+        }
+    }
+
+    private class InsertTask extends AsyncTask<ContentValues, Void, Void> {
+        private Cursor constantsCursor = null;
+
+        @Override
+        protected Void doInBackground(ContentValues... values) {
+            db.getWritableDatabase().insert(DatabaseHelper.TABLE,
+                    DatabaseHelper.TITLE, values[0]);
+
+            constantsCursor = doQuery();
+            constantsCursor.getCount();
+
+            return (null);
+        }
+
+        @Override
+        public void onPostExecute(Void arg0) {
+            cursorAdapter.changeCursor(constantsCursor);
+
+        }
+    }
+
+    private class DeleteTask extends AsyncTask<String, Void, Void> {
+        private Cursor constantsCursor = null;
+
+        @Override
+        protected Void doInBackground(String... values) {
+            db.getWritableDatabase().delete(DatabaseHelper.TABLE, "_id=?",
+                    new String[] { values[0] });
+
+            constantsCursor = doQuery();
+            constantsCursor.getCount();
+
+            return (null);
+        }
+
+        @Override
+        public void onPostExecute(Void arg0) {
+            cursorAdapter.changeCursor(constantsCursor);
+
+        }
+    }
+
+
+    AdapterView.OnItemLongClickListener itemLongClickListener = new AdapterView.OnItemLongClickListener() {
+
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                       int position, long id) {
+            Cursor cursor = (Cursor) cursorAdapter.getItem(position);
+            int constantId = cursor.getInt(cursor.getColumnIndex("_id"));
+            new DeleteTask().execute(constantId + "");
+
+            return false;
+        }
+    };
+
+}
---
+++ beginner/database/ConstantsSample2/app/src/main/java/com/training/constantssample2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.training.constantssample2;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.container) == null) {
+            FragmentTransaction fragmentTransaction = fragmentManager
+                    .beginTransaction();
+            fragmentTransaction.add(R.id.container, new ConstantsFragment());
+            fragmentTransaction.commit();
+
+        }
+    }
+
+
+}
